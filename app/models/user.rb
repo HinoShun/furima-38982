5614e79e25cfb42de09297a6afd0deee364614a9
HinoShun <shumpei.hino88@icloud.com>
2023-03-03 18:31:04 +0900

    ユーザー管理機能・ご指摘事項修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
 
   #has_many :items
   #has_many :orders
+  
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/, message: "Include both letters and numbers"}
 
   with_options presence: true do
     validates :nickname
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/, message: "Include both letters and numbers"}
     validates :family_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Input full-width characters"}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Input full-width characters"}
     validates :family_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "Input full-width katakana characters"}
