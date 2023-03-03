a10695c752d9b562f6041082a0c92ffa9f19e2ee
HinoShun <shumpei.hino88@icloud.com>
2023-03-04 03:33:13 +0900

    金額計算表示（JS）の実装

---
+++ app/javascript/calculation.js
@@ -0,0 +1,16 @@
+function calculation (){
+  const input = document.getElementById("item-price");
+  input.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    const itemPrice = input.value
+    const taxRate = 0.1;
+    const commission = Math.floor(itemPrice * taxRate);
+    const profit = Math.floor(itemPrice - commission);
+    taxInput = document.getElementById("add-tax-price")
+    taxInput.innerHTML = `${commission}`
+    profitInput = document.getElementById("profit")
+    profitInput.innerHTML = `${profit}`
+  });
+};
+ 
+ window.addEventListener('load', calculation);
