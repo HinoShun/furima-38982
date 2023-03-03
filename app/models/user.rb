5eb442777fd5970c7d1b061a3d5c239d1559fa82
HinoShun <shumpei.hino88@icloud.com>
2023-03-03 15:39:12 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  #has_many :orders
+
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/, message: "Include both letters and numbers"}
+    validates :family_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Input full-width characters"}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Input full-width characters"}
+    validates :family_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "Input full-width katakana characters"}
+    validates :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "Input full-width katakana characters"}
+    validates :birthday
+  end
+
 end
